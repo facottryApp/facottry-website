da26981fa304e94c5623311782bf779d566b2e3b
Styles <bcs_2021035@iiitm.ac.in>
2023-08-19 16:40:34 +0530

    conditional rendering of NavBar

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+type Props = {}
+
+const page = ({}: Props) => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-neutral-100">
+      <p className="text-7xl font-extrabold text-yellow-600">Dashboard</p>
+    </div>
+  )
+}
+
+export default page
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,8 @@ import logo from '@/assets/logo.jpg'
 import Link from "next/link"
 
 const NavBar = () => {
+    const isLoggedin = true;
+
     return (
         <div className="p-5 flex justify-between items-center max-w-7xl m-auto">
             <Link href="/">
@@ -16,14 +18,25 @@ const NavBar = () => {
                 />
             </Link>
 
-            <div className="flex font-medium gap-8 items-center">
-                <div className="text-md flex gap-6">
-                    <button className="hover:text-yellow-600 transition">About Us</button>
-                    <button className="hover:text-yellow-600 transition">Pricing</button>
-                    <Link href="/docs" className="hover:text-yellow-600 transition">Documentation</Link>
+            {!isLoggedin ? (
+                <div className="flex font-medium gap-8 items-center">
+                    <div className="text-md flex gap-6">
+                        <button className="hover:text-yellow-600 transition">About Us</button>
+                        <button className="hover:text-yellow-600 transition">Pricing</button>
+                        <Link href="/docs" className="hover:text-yellow-600 transition">Documentation</Link>
+                    </div>
+                    <button className="border rounded-xl p-2 hover:bg-yellow-600 transition-all hover:text-white">Sign In</button>
+                </div>) : (
+                <div className="flex font-medium gap-8 items-center">
+                    <div className="text-md flex gap-6">
+                        <Link href="/docs" className="hover:text-yellow-600 transition">Dashboard</Link>
+                    </div>
+                    <div className="text-md flex gap-6">
+                        <Link href="/docs" className="hover:text-yellow-600 transition">Docs</Link>
+                    </div>
+                    <button className="border rounded-xl p-2 hover:bg-yellow-600 transition-all hover:text-white">Sign Out</button>
                 </div>
-                <button className="border rounded-xl p-2 hover:bg-yellow-600 transition-all hover:text-white">Sign In</button>
-                </div>
+            )}
         </div>
     )
 }
