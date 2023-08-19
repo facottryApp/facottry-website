516ec01381b542e0704e5f6a145e0ddcaba64249
Styles <bcs_2021035@iiitm.ac.in>
2023-08-19 16:25:37 +0530

    added logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image"
 import React from 'react'
+import logo from '@/assets/logo.jpg'
 
 const NavBar = () => {
     return (
         <div className="p-5 flex justify-between items-center max-w-7xl m-auto">
-            <p className="text-2xl font-extrabold text-purple-900">FacOTTry</p>
+            <Image
+                src={logo}
+                width={150}
+                height={100}
+                alt="logo"
+                className="rounded-2xl"
+            />
+
             <div className="flex font-medium gap-8 items-center">
                 <div className="text-md flex gap-6">
                     <button className="hover:text-purple-600 transition">About Us</button>
@@ -11,7 +20,7 @@ const NavBar = () => {
                     <button className="hover:text-purple-600 transition">Documentation</button>
                 </div>
                 <button className="border rounded-xl p-2 hover:bg-purple-600 transition-all hover:text-white">Sign In</button>
-            </div>
+                </div>
         </div>
     )
 }
