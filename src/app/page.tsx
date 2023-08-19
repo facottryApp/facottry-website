a475e4b4e510be4f2da16e922ed8727a65a15f69
Styles <bcs_2021035@iiitm.ac.in>
2023-08-19 15:47:52 +0530

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+type Props = {}
+
+const page = ({}: Props) => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-neutral-100">
+      <p className="text-7xl font-extrabold text-purple-900">FacOTTry</p>
+    </div>
+  )
+}
+
+export default page
