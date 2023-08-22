e516d1a2d8324ec67d83278b1d1a27b866e2f3da
Styles <bcs_2021035@iiitm.ac.in>
2023-08-22 17:52:36 +0530

    config changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { LandingPage } from "@/components/Landing"
+
 type Props = {}
 
 const page = ({}: Props) => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-100">
-      <p className="text-7xl font-extrabold text-yellow-600">Home</p>
+    <div className="">
+      <LandingPage />
     </div>
   )
 }
