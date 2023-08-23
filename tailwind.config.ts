7adeebf281c8a820a89494270a3524937d68d3f7
Styles <bcs_2021035@iiitm.ac.in>
2023-08-23 13:53:20 +0530

    FAQ page structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,21 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        primary100: '#D5E6F5',
+        primary200: '#ACCDEB',
+        primary300: '#83B4E1',
+        primary400: '#5A9BD7',
         primary: '#3182CE',
+        primary600: '#2768A4',
+        primary700: '#1D4E7B',
+        primary800: '#133452',
+        primary900: '#0E273D',
+
         bgblue: "#F7FAFC",
         bgblue200: "#E2EDF5",
         darkblue: "#132537",
-        darkblue200: "#00162D",
+        darkblue200: "#042444",
+        darkblue300: "#00162D",
       },
     },
   },
