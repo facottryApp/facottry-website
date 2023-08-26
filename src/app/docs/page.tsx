be5d1c2d76307512ae48a4726c1a5ed99b212823
Styles <bcs_2021035@iiitm.ac.in>
2023-08-27 03:48:46 +0530

    main document home

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,9 +1,10 @@
+import Documentation from "@/components/docs/Main"
 import React from 'react'
 
 const page = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-100">
-      <p className="text-7xl font-extrabold text-yellow-600">Documentation</p>
+    <div>
+      <Documentation />
     </div>
   )
 }
