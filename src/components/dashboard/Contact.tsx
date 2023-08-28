47fe5faa3e16b9acdcb0c9dfcfea9397d1d52727
Styles <bcs_2021035@iiitm.ac.in>
2023-08-28 13:09:04 +0530

    added contact page

---
+++ src/components/dashboard/Contact.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Sidebar from "./Sidebar"
+import { FiEdit3 } from "react-icons/fi"
+import ToggleSwitch from "../common/ToggleTheme"
+import logo from '@/assets/logo_1.svg'
+import Image from 'next/image'
+import { BsChevronDown } from 'react-icons/bs';
+import ContactForm from "../common/ContactForm";
+
+const Pricing = () => {
+    return (
+        <section className="flex min-h-screen bg-bggray dark:bg-darkblue300">
+            <Sidebar />
+            <div className="w-full py-8 px-8 mx-auto ">
+                <nav className="flex justify-between">
+                    <div className="flex items-center mr-10 space-x-4">
+                        <h1 className="text-2xl font-bold">Config Dashboard</h1>
+
+                        <button className="flex items-center gap-2 p-2 ml-2 font-medium text-white transition-all rounded-lg bg-primary hover:bg-primary400">
+                            <FiEdit3 fontSize='1.3rem' />
+                            <p>Edit Config</p>
+                        </button>
+                    </div>
+
+                    <div className="flex items-center gap-6">
+                        <ToggleSwitch />
+
+                        <button className="flex items-center ml-2">
+                            <Image src={logo} alt="user" width={40} height={40} className="p-1 bg-gray-200 rounded-full" />
+                            <p className="ml-3 mr-2 text-lg font-semibold">Kartik</p>
+                            <BsChevronDown />
+                        </button>
+                    </div>
+                </nav>
+
+                <hr className="w-full mt-4" />
+
+                <div className="mt-8">
+                    <ContactForm />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Pricing;
