1c5565ef68842ae7d67b5edd8a706908280daaf3
Styles <bcs_2021035@iiitm.ac.in>
2023-09-01 14:33:37 +0530

    Update ToggleButton.tsx

--- src/components/common/ToggleButton.tsx
+++ src/components/common/ToggleButton.tsx
@@ -9,7 +9,7 @@ export const ToggleButton = () => {
         <Switch
             checked={enabled}
             onChange={setEnabled}
-            className={`bg-black relative inline-flex h-[28px] w-[52px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+            className={`bg-slate-800 relative inline-flex h-[28px] w-[52px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 scale-75`}
         >
             <span className="sr-only">Theme Switch</span>
             <span
